64d33d5cad8cd12676afe33571f2d0a6e07c7e1d
Rafael Batista <rafaelsbatista@gmail.com>
2015-12-20 18:06:38 -0200

    Function to check if meditation is viewed

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,30 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("isMeditationViewed", function(request, response) {
+	var query = new Parse.Query("UserView");
+	query.equalTo("meditation",{
+        __type: "Pointer",
+        className: "meditations",
+        objectId: request.params.meditationId
+    });
+	query.equalTo("user", {
+        __type: "Pointer",
+        className: "_User",
+        objectId: request.params.userId
+    });
+	
+	query.find({
+		success: function(results) {
+			if (results.length > 0) {
+				response.success(true);
+			} else {
+				response.success(false);
+			}
+		},
+		error: function() {
+			response.error("Could not execute query");
+		}
+	});
+});

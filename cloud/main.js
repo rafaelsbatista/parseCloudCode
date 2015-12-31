efd170fc001eb1de995178026cdd908b38a66455
Rafael Batista <rafaelsbatista@gmail.com>
2015-12-31 19:02:17 -0200

    New function to determine how many meditations was viewed by the user

--- cloud/main.js
+++ cloud/main.js
@@ -51,3 +51,49 @@ Parse.Cloud.define("getMeditationsViewed", function(request, response) {
 		}
 	});
 });
+
+Parse.Cloud.define("CountMeditationsViewed", function(request, response) {
+	var queryView = new Parse.Query("UserView");
+	queryView.equalTo("user", {
+        __type: "Pointer",
+        className: "_User",
+        objectId: request.params.userId
+    });
+	
+	
+	queryView.find({
+		success: function(results) {
+			
+			var meditationsViewedIds = [];
+			for (var i = 0 ; i < results.length ; i++) {
+				meditationsViewedIds.push(results[i].get("meditation").id);
+			}
+			
+			
+			var queryGuide = new Parse.Query("meditations");
+			queryGuide.find({
+				success: function(results) {
+					var result = {};
+					for (var i = 0 ; i < results.length ; i++) {
+						var guideId = results[i].get("guide").id; 
+						var meditationId = results[i].id;
+						if (!result[guideId]) {
+							result[guideId] = {count: 0, total: 0};
+						}
+						result[guideId].total += 1;
+						if (meditationsViewedIds.indexOf(meditationId) != -1) {
+							result[guideId].count += 1;
+						} 
+					}
+					response.success(result);
+				},
+				error: function() {
+					response.error("Could not execute queryGuide");
+				}
+			});
+		},
+		error: function() {
+			response.error("Could not execute queryView");
+		}
+	});
+});
